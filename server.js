ec2160697a7cb6eb401d06f256a9e6612dfeabd1
Harkirat Singh <harkirat96@gmail.com>
2015-09-13 21:31:27 +0530

    Added city wise distribution

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 
 var files_map = {
 	"category" : "./data/state_city_category_count.json",
-	"category_list" : "./data/category_list.json"
+	"category_list" : "./data/category_list.json",
+	"cities_list" :  "./data/city.json"
 }
 
 app.use(express.static('public'));
@@ -20,6 +21,53 @@ app.get('/category/:id', function(req, res){
 	}
 	res.end(JSON.stringify(outData));
 });
+
+
+app.get('/cities', function(req, res){
+	var category_id = req.params.id;
+	var cities = require(files_map["cities_list"]);
+	var catergoryList = require(files_map["category"]);
+	var outData = {};
+	for(city in cities) {
+	//	console.log(city);
+	//	console.log(cities[city]);	
+	//	console.log(cities[city].name);
+		for(categoryl in catergoryList)
+		{
+			for(something in catergoryList[categoryl])
+			{
+				if(something.toLowerCase() === cities[city].name.toLowerCase())
+				{
+					//	console.log("hi");
+					console.log(catergoryList[categoryl][something]);
+					console.log()
+					outData[something.toLowerCase()] = 0;
+					outData[something.toLowerCase()] = {};
+					outData[something.toLowerCase()].name = (cities[city].name.toLowerCase());
+					outData[something.toLowerCase()].val1 = (catergoryList[categoryl][something][71]);
+					outData[something.toLowerCase()].val2 = (catergoryList[categoryl][something][72]);
+					outData[something.toLowerCase()].val3 = (catergoryList[categoryl][something][147]);
+					outData[something.toLowerCase()].val4 = (catergoryList[categoryl][something][149]);
+					outData[something.toLowerCase()].lat = (cities[city].lat);
+					outData[something.toLowerCase()].lon = (cities[city].lon);
+			//		console.log("----");
+			//		console.log(outData[something.toLowerCase()].val1);
+			//		console.log(outData[something.toLowerCase()].val2);
+			//		console.log(outData[something.toLowerCase()].val3);
+			//		console.log(outData[something.toLowerCase()].val4);
+			//		console.log(outData[something.toLowerCase()].lat);
+			//		console.log(outData[something.toLowerCase()].lon);
+					break;
+				
+				}
+			}
+			// break;	
+		}
+	}
+	console.log(outData);
+	res.end(JSON.stringify(outData));
+});
+
 
 app.get('/list/category', function(req, res){
 	var data = require(files_map["category_list"]);
